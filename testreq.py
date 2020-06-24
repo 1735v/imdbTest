0a119a5a5569ba383328beb883252750f4eb61ad
1735v <44443721+1735v@users.noreply.github.com>
2020-06-24 16:03:03 +0300

    Add files via upload
    ENGLISH <3

--- testreq.py
+++ testreq.py
@@ -5,13 +5,14 @@ import requests
 
 class SearchMovieTest(unittest.TestCase):
 
+    #We check the server respons before each test
     #перевіряєм перед кожним тестом чи сервер відповідає
     def setUp(self):
         print('Test start '+ str(requests.get('http://www.omdbapi.com/').status_code))
 
-    #перевірка формату
 
 
+    #We  check the ability to search a movie by title
     #перевіряєм чи знаходить фільм по точній назві
     def test_search_movie(self):
         self.assertEqual(search_movie('Knives Out')['Title'],'Knives Out')
@@ -20,12 +21,14 @@ class SearchMovieTest(unittest.TestCase):
     def test_search_movie_bad_name(self):
         self.assertNotEqual(search_movie('Knives Out')['Title'], 'name movie')
 
+
+    #We check search capability each movies by words
     #перевіряєм чи знаходить N-у кількість результатів по якомусь слову
     def test_earch_movie_list(self):
         pages=int(search_movie_list('Knives')['totalResults'])
         self.assertGreaterEqual(pages,14)
 
-
+    #We check pagination work
     #перевіряємо чи пагінація норм працює чи намає однакових фільмів на різних сторінках
     def test_search_movie_pages(self):
 
@@ -33,6 +36,7 @@ class SearchMovieTest(unittest.TestCase):
         page2 = search_movie_pages('Knives','2')['Search'][1]
         self.assertFalse(page1==page2)
 
+    #We check work of the  work of the function of finding the rating of the film
     def test_search_movie_ratings(self):
         self.assertIsInstance(search_movie_ratings('Knives Out'),float)
 
