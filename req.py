9c8c93c6689d0fe0775cf6885b87dbef867c343d
1735v <44443721+1735v@users.noreply.github.com>
2020-06-24 13:11:05 +0300

    Add files via upload
    #2

--- req.py
+++ req.py
@@ -1,23 +1,35 @@
 import requests
 
-#API_KEY = 'd1149b8f'
-#url = 'http://www.omdbapi.com/'
-#params = {
-#    't' : 'Knives Out',
-#    'p' : 'full'
-#}
+#API_KEY = &apikey=d1149b8f
+
 
 def search_movie(name):
     url = 'http://www.omdbapi.com/'
     para ={'apikey': 'd1149b8f' ,'t': name}
     respons = requests.get(url, params=para)
 
-    return (respons.json()['Title'])
-
-print(search_movie('Knives Out'))
+    return (respons.json())
 
 
+def search_movie_list(name):
+    url = 'http://www.omdbapi.com/'
+    para = {'apikey': 'd1149b8f', 's': name}
+    respons = requests.get(url, params=para)
 
-#respons = requests.get(url, params=params)
-#r = respons.json()
-#print(r)
+    return (respons.json())
+
+
+#print(search_movie_list('Knives'))
+
+
+
+
+def search_movie_pages(name,page=1):
+    url = 'http://www.omdbapi.com/'
+    para ={'apikey': 'd1149b8f' ,'s': name,'page':page}
+    respons = requests.get(url, params=para)
+
+    return (respons.json())
+
+
+#print(search_movie_pages('Knives')['Search'][1]==search_movie_pages('Knives')['Search'][1])
--- testreq.py
+++ testreq.py
@@ -1,13 +1,42 @@
-from .req import search_movie
+from .req import *
 import unittest
+import requests
+
 
 class SearchMovieTest(unittest.TestCase):
 
+    #перевіряєм перед кожним тестом чи сервер відповідає
+    def setUp(self):
+        print('Test start '+ str(requests.get('http://www.omdbapi.com/').status_code))
+
+    #перевіряєм чи знаходить фільм по точній назві
     def test_search_movie(self):
-        self.assertEqual(search_movie('Knives Out'),'Knives Out')
-        self.assertNotEqual(search_movie('Knives Out'),'name movie')
+        self.assertEqual(search_movie('Knives Out')['Title'],'Knives Out')
+        self.assertNotEqual(search_movie('Knives Out')['Title'],'name movie')
+
+    def test_search_movie_bad_name(self):
+        self.assertNotEqual(search_movie('Knives Out')['Title'], 'name movie')
+
+    #перевіряєм чи знаходить N-у кількість результатів по якомусь слову
+    def test_earch_movie_list(self):
+        pages=int(search_movie_list('Knives')['totalResults'])
+        self.assertGreaterEqual(pages,14)
+
+
+    #перевіряємо чи пагінація норм працює чи намає однакових фільмів на різних сторінках
+    def test_search_movie_pages(self):
+
+        page1 = search_movie_pages('Knives')['Search'][1]
+        page2 = search_movie_pages('Knives','2')['Search'][1]
+        self.assertFalse(page1==page2)
+
+    def tearDown(self):
+        print('test finished')
+
 
 
 
 if __name__ == '__main__':
     unittest.main()
+
+
